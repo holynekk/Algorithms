a409f8e8247b2488c7e28c747c478647376c7ed5
HolyNekK <e238109@metu.edu.tr>
2020-07-19 22:14:19 +0300

    insertion sort

--- Sorting/Insertion_Sort/insertion_sort.c
+++ Sorting/Insertion_Sort/insertion_sort.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main(void){
-
-	return 0;
+void insertion sort(int *arr, int n){
+	int i, j, main;
+	for (i = 1; i < n; i++){
+		main = arr[i];
+		j = i - 1;
+		while (j >= 0 && arr[j] > main){
+			arr[j+1] = arr[j];
+			j -= 1;
+		}arr[j+1] = main;
+	}
 }
