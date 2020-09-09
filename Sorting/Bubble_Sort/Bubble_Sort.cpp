bd5a433baac8815104479fae655cb518cc322cdd
HolyNekK <e238109@metu.edu.tr>
2020-09-09 19:52:30 +0300

    Modified

--- Sorting/Bubble_Sort/Bubble_Sort.cpp
+++ Sorting/Bubble_Sort/Bubble_Sort.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 void bubble_sort(int array[], int n){
-    for(int i = 0; i < n; i++){
-        for (int j = 0; j < n-1; j++){
-            if (array[j] < array[j+1]){
-                swap(array[j], array[j+1])
+    for(int i = 0; i < n - 1; i++){
+        for (int j = 0; j < n-i-1; j++){
+            if (array[j] > array[j+1]){
+                swap(array[j], array[j+1]);
             }
         }
     }
