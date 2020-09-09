7f0a15814e065af7d56aac7183db406b5535b473
HolyNekK <e238109@metu.edu.tr>
2020-09-09 20:46:47 +0300

    Binary search

--- Search/Binary_Sarch/binary_search.cpp
+++ Search/Binary_Sarch/binary_search.cpp
@@ -2,7 +2,19 @@
 
 using namespace std;
 
+// Funtion returns the index of the number
+// that we search for
 int binary_search(int arr[], int number){
-
-
+    int start = 0, end = number - 1;
+    while (start <= end){
+        int mid = (start + end)/2;
+        if (arr[mid] == number){
+            return mid;
+        }if (arr[mid] > number){
+            end = mid - 1;
+        }else{
+            start = mid + 1;
+        }
+    }
+    
 }
